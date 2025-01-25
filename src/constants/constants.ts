335e9f08a2ed859f60363aab93015b374852a939
kamal <sonikamal99505@gmail.com>
2025-01-25 20:33:14 +0530

    gh pages image ur

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,5 @@
 export const COMPANY_NAME = 'Central Texas Fly Fishing';
-export const LOGO_URL = '/images/logo.svg';
+export const LOGO_URL = '/figma_task/images/logo.svg';
 export const LOGO_ALT = 'Central Texas Fly Fishing Logo';
 
 export const NAV_LINKS = [
@@ -10,9 +10,9 @@ export const NAV_LINKS = [
 ];
 
 export const SOCIAL_LINKS = [
-  { iconUrl: '/images/fb.svg',iconAlt:"fb", href: 'https://facebook.com', ariaLabel: 'Visit our Facebook page' },
-  { iconUrl: '/images/insta.svg',iconAlt:"insta", href: 'https://instagram.com', ariaLabel: 'Visit our Instagram page' },
-  { iconUrl: '/images/linkedin.svg',iconAlt:"linkedin", href: 'https://linkedin.com', ariaLabel: 'Visit our LinkedIn page' }
+  { iconUrl: '/figma_task/images/fb.svg',iconAlt:"fb", href: 'https://facebook.com', ariaLabel: 'Visit our Facebook page' },
+  { iconUrl: '/figma_task/images/insta.svg',iconAlt:"insta", href: 'https://instagram.com', ariaLabel: 'Visit our Instagram page' },
+  { iconUrl: '/figma_task/images/linkedin.svg',iconAlt:"linkedin", href: 'https://linkedin.com', ariaLabel: 'Visit our LinkedIn page' }
 ];
 
 export const FOOTER_LINKS = [
@@ -26,7 +26,7 @@ export const HERO_CONTENT = {
   title: 'Central Texas\nFly Fishing',
   description: 'At lacus vitae nulla sagittis scelerisque nisl. Pellentesque duis cursus vestibulum, facilisi ac, sed faucibus.',
   ctaText: 'Get started',
-  imageUrl: '/images/hero.png',
+  imageUrl: '/figma_task/images/hero.png',
   imageAlt: 'Fly fishing in Central Texas'
 };
 
@@ -34,20 +34,20 @@ export const FEATURES = [
   {
     title: 'Explore Fly Fishing',
     description: 'Proin nibh nisl condimentum id venenatis a condimentum vitae sapien. Tellus in metus vulputate eu sc',
-    imageUrl: '/images/fish1.png',
+    imageUrl: '/figma_task/images/fish1.png',
     imageAlt: 'Person fly fishing',
     isOpen: true
   },
   {
     title: 'Fly Fishing Experiences',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.',
-    imageUrl: '/images/fish4.png',
+    imageUrl: '/figma_task/images/fish4.png',
     imageAlt: 'Scenic fly fishing location'
   },
   {
     title: 'Gear Up and Catch More',
     description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.',
-    imageUrl: '/images/fish5.png',
+    imageUrl: '/figma_task/images/fish5.png',
     imageAlt: 'Fly fishing gear'
   }
 ];
@@ -56,25 +56,25 @@ export const STORIES = [
   {
     title: "John's Story",
     description: 'Non arcu risus quis varius quam quisque. Ornare suspendisse sed nisi lacus. Eu feugiat pretium nibh',
-    imageUrl: '/images/story1.png',
+    imageUrl: '/figma_task/images/story1.png',
     imageAlt: "John's fly fishing adventure"
   },
   {
     title: 'The Journey',
     description: 'Proin nibh nisl condimentum id venenatis a condimentum vitae sapien. Tellus in metus vulputate eu sc',
-    imageUrl: '/images/story2.png',
+    imageUrl: '/figma_task/images/story2.png',
     imageAlt: 'Fly fishing journey'
   },
   {
     title: 'Catch Day',
     description: 'Elit pellentesque habitant morbi tristique senectus et netus et. In hendrerit gravida rutrum quisque',
-    imageUrl: '/images/fish2.png',
+    imageUrl: '/figma_task/images/fish2.png',
     imageAlt: 'Successful catch day'
   },
   {
     title: 'Trout Tales',
     description: 'Sit amet mattis vulputate enim nulla aliquet. At augue eget arcu dictum varius. Volutpat commodo sed',
-    imageUrl: '/images/fish3.png',
+    imageUrl: '/figma_task/images/fish3.png',
     imageAlt: 'Trout fishing experience'
   }
 ];
